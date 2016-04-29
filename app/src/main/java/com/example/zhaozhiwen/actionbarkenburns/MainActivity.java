b7e83f58f1ab0fcd31598a20b23b36cba453fd45
zhaozhiwen <1135594680@qq.com>
2016-04-29 11:16:54 +0800

    就是让你的Actionbar变得不那么单调

---
+++ app/src/main/java/com/example/zhaozhiwen/actionbarkenburns/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.zhaozhiwen.actionbarkenburns;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this, NoBoringActionBarActivity.class));
+
+            }
+        });
+    }
+
+
+}
+
